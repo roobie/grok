1801d3c47142ce2fec159c5a29e89f71abdc3224
bjorn roberg <bjorn.roberg@gmail.com>
2019-03-23 00:44:06 +0100

    init

---
+++ Grok.Main/src/Grok.Cli/Program.fs
@@ -0,0 +1,14 @@
+﻿// Learn more about F# at http://fsharp.org
+
+open System
+open Grok.Lib
+
+[<EntryPoint>]
+let main argv =
+    printfn "Nice command-line arguments! Here's what JSON.NET has to say about them:"
+
+    argv
+    |> Array.map Library.getJsonNetJson
+    |> Array.iter (printfn "%s")
+
+    0 // return an integer exit code
---
+++ Grok.Main/src/Grok.Lib/Library.fs
@@ -0,0 +1,7 @@
+﻿namespace Grok.Lib
+
+open Newtonsoft.Json
+
+module Library =
+  let getJsonNetJson value =
+      sprintf "I used to be %s but now I'm %s thanks to JSON.NET!" value (JsonConvert.SerializeObject(value))
